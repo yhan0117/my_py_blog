cb2e175f6e5f207cdffe3ae5696bd61ffaaea5df
han <you@example.com>
2023-05-05 13:40:41 -0400

    Update Blog

--- myblog-venv/pelicanconf.py
+++ myblog-venv/pelicanconf.py
@@ -1,4 +1,4 @@
-AUTHOR = 'han'
+AUTHOR = 'HAN YANG'
 SITENAME = 'HanYang_Blog'
 SITEURL = ''
 
@@ -6,7 +6,7 @@ PATH = 'content'
 
 TIMEZONE = 'EST'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -16,14 +16,12 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Youtube', 'https://studio.youtube.com/channel/UC5S8P4F6j_xLxhJU8zC8rZQ/videos/upload?filter=%5B%5D&sort=%7B%22columnType%22%3A%22date%22%2C%22sortOrder%22%3A%22DESCENDING%22%7D'),
+         ('Python.org', 'https://www.python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Linkedin', 'https://www.linkedin.com/in/han-yang-522270223/'),
+          ('Github', 'https://github.com/yhan0117/my_py_blog'),)
 
 DEFAULT_PAGINATION = 10
 
